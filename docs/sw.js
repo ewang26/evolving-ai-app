1d3be4ae110b8445c648834af533fda0db0f7e00
ewang26 <erikw789@gmail.com>
2026-09-18 22:01:58 -0700

    Bump cache

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* Offline shell. Bump CACHE on every deploy so clients pick up the new build. */
-var CACHE = "eai-v27";
+var CACHE = "eai-v28";
 var ASSETS = ["./", "./index.html", "./config.js", "./manifest.webmanifest",
               "./icons/icon-192.png", "./icons/apple-touch-icon.png",
               "./icons/benchmark-favicon.svg"];
