82ead41e73d380fc0e5e7ec2307ea78748f83023
ewang26 <erikw789@gmail.com>
2026-09-19 09:54:14 -0700

    Everyone reads the Evolvable AI paper; Axelrod & Hamilton replaces Ostrom on topic ii

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* Offline shell. Bump CACHE on every deploy so clients pick up the new build. */
-var CACHE = "eai-v30";
+var CACHE = "eai-v31";
 var ASSETS = ["./", "./index.html", "./config.js", "./manifest.webmanifest",
               "./icons/icon-192.png", "./icons/apple-touch-icon.png",
               "./icons/benchmark-favicon.svg"];
