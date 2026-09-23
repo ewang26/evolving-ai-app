7320df476884892ea5e040c8999171059794f28d
ewang26 <erikw789@gmail.com>
2026-09-22 19:32:09 -0700

    Use in-page moderation confirmation and support iOS dialogs

--- ios/EvolvingAI/WebAppController.swift
+++ ios/EvolvingAI/WebAppController.swift
@@ -286,6 +286,16 @@ extension WebAppController: WKUIDelegate {
         alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in completionHandler() })
         present(alert, animated: true)
     }
+
+    func webView(_ webView: WKWebView,
+                 runJavaScriptConfirmPanelWithMessage message: String,
+                 initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping (Bool) -> Void) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { _ in completionHandler(false) })
+        alert.addAction(UIAlertAction(title: "Confirm", style: .default) { _ in completionHandler(true) })
+        present(alert, animated: true)
+    }
 }
 
 // MARK: - Messages from the page
